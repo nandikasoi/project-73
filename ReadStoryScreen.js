54bcfad7b0fbe0f8f3a7c763648e0b485b28b581
nandikasoi <69952137+nandikasoi@users.noreply.github.com>
2021-04-28 13:29:28 +0530

    Add files via upload

---
+++ ReadStoryScreen.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import { StyleSheet, Text, View ,FlatList} from 'react-native';
+import db from '../config'
+
+
+
+
+export default class ReadStoryScreen extends React.Component {
+  constructor(){
+    super();
+    this.state ={
+      allStories:[]
+    }
+  }
+  componentDidMount(){
+    this.retrieveStories()
+  }
+
+  retrieveStories=()=>{
+    try {
+      var allStories= []
+      var stories = db.collection("stories")
+        .get().then((querySnapshot)=> {
+          querySnapshot.forEach((doc)=> {
+              // doc.data() is never undefined for query doc snapshots
+              
+              allStories.push(doc.data())
+              console.log('this are the stories',allStories)
+          })
+          this.setState({allStories})
+        })
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+  searchTransactions= async(text) =>{
+    var enteredText = text.split("")  
+    if (enteredText[0].toUpperCase() ==='B'){
+      const transaction =  await db.collection("transactions").where('bookId','==',text).get()
+      transaction.docs.map((doc)=>{
+        this.setState({
+          allTransactions:[...this.state.allTransactions,doc.data()],
+          lastVisibleTransaction: doc
+        })
+      })
+    }
+    else if(enteredText[0].toUpperCase() === 'S'){
+      const transaction = await db.collection('transactions').where('studentId','==',text).get()
+      transaction.docs.map((doc)=>{
+        this.setState({
+          allTransactions:[...this.state.allTransactions,doc.data()],
+          lastVisibleTransaction: doc
+        })
+      })
+    }
+  }
+
+    render(){
+        return(
+                 <FlatList
+                    data={this.state.allStories}
+                    renderItem={({ item }) => (
+                      <View style={styles.itemContainer}>
+                        <Text>Title: {item.title}</Text>
+                    <Text>Author : {item.author}</Text>
+                      </View>
+                    )}
+                    keyExtractor={(item, index) => index.toString()}
+                    />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },item: {
+    backgroundColor: 'pink',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+  searchBar:{
+    flexDirection:'row',
+    height:40,
+    width:'auto',
+    borderWidth:0.5,
+    alignItems:'center',
+    backgroundColor:'grey',
+  },
+   itemContainer: {
+    height: 80,
+    width:'100%',
+    borderWidth: 2,
+    borderColor: 'pink',
+    justifyContent: 'center',
+    alignSelf: 'center',
+  }
+});
